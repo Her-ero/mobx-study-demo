11473bee4d9c5b7280932363ad708fc9c3090a48
Alex <alex@jiaozhenhr.com>
2018-07-03 17:15:12 +0800

    update

--- src/components/todoEntry.js
+++ src/components/todoEntry.js
@@ -1,48 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import {observer} from 'mobx-react';
+import React from 'react'
+// import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import {observer} from 'mobx-react'
 
-const ENTER_KEY = 13;
+const ENTER_KEY = 13
 
 @observer
 export default class TodoEntry extends React.Component {
 
-	// handleChange(ev) {
-	// 	console.log(ev.target.value)
-	// 	console.log(ev.keyCode)
-		
-	// 	ev.preventDefault()
-
-	// 	const val = ev.target.value.trim()
-
-	// 	if (val) {
-	// 		this.props.todoStore.addTodo(val)
-	// 		this.textInput.value = ''
-	// 	}
-
-	// }
-
-	// handleSubmit(event) {
-	// 	console.log(event.target.value)
-	// 	event.preventDefault();
-	// }
-
-	render() {
-		return (<input
-			// ref="newField"
-			ref={(i) => {this.textInput = i}}
-			className="new-todo"
-			placeholder="What needs to be done?"
-			onKeyDown={this.handleNewTodoKeyDown}
-			onChange={this.handleChange}
-			autoFocus={true}
-		/>)
-	}
-
 	handleNewTodoKeyDown = (ev) => {
 
-		console.log(this.textInput.value)
+		// console.log(this.textInput.value)
 		if (ev.keyCode !== ENTER_KEY) {
 			return
 		}
@@ -55,6 +23,17 @@ export default class TodoEntry extends React.Component {
 			this.props.todoStore.addTodo(val)
 			this.textInput.value = ''
 		}
+	}
+
+	render() {
+		return (<input
+			ref={(i) => {this.textInput = i}}
+			className="new-todo"
+			placeholder="What needs to be done?"
+			onKeyDown={this.handleNewTodoKeyDown}
+			onChange={this.handleChange}
+			autoFocus={true}
+		/>)
 	}
 }
 
